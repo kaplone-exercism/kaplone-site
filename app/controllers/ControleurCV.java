5a8259bf88d33cc50d2ad69abd375e3ccc69292a
David <dvd.brtrnd@gmail.com>
2017-03-21 19:56:44 +0100

    introduction routes CV

--- app/controllers/ControleurCV.java
+++ app/controllers/ControleurCV.java
@@ -12,7 +12,7 @@ import play.mvc.*;
 import play.mvc.Http.*;
 
 import views.html.*;
-import enums.*;
+//import enums.*;
 
 import play.twirl.api.Html;
 
@@ -20,19 +20,20 @@ public class ControleurCV extends Controller {
 
    public Result afficher(String article) {
 
-       Articles a = null;
-
-       try {
-           a = Articles.valueOf(article);
-       }
-       catch (IllegalArgumentException iae){
-           a = Articles.valueOf("architecte");
-       }
+//       Articles a = null;
+//
+//       try {
+//           a = Articles.valueOf(article);
+//       }
+//       catch (IllegalArgumentException iae){
+//           a = Articles.valueOf("architecte");
+//       }
 
        // http://www.kaplone.fr/assets/articles/
 
        return ok(main.render(article,
-               Html.apply(a.getFileContent())));
+              // Html.apply(a.getFileContent())));
+               Html.apply(new String(Files.readAllBytes(DefaultApplication.getFile(String.format("/public/articles/%s.html")).toPath())))));
 
     }
 }
