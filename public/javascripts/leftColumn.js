1634e95a588503da64aa0662d4353167a28ebfae
David <dvd.brtrnd@gmail.com>
2017-03-22 18:02:39 +0100

    introduction routes CV

--- public/javascripts/leftColumn.js
+++ public/javascripts/leftColumn.js
@@ -1,22 +1,25 @@
 $(document).ready(function () {
     $('label.tree-toggler').click(function () {
+        $('ul.tree').hide();
         $(this).parent().children('ul.tree').toggle(600);
     });
 });
 
 $(document).ready(function () {
-    $('ul.tree').click(function () {
-        var out;
-        var level;
+    $('ul.tree').hide();
 
-        addEventListener("turbolinks:before-render", function() {
-            out = document.getElementById("scroll");
-            level = out.scrollTop;
-            alert(level);
-        });
-
-        addEventListener("turbolinks:render", function() {
-            out.scrollTop = level;
-        });
-    });
+//    $('ul.tree').click(function () {
+//        var out;
+//        var level;
+//
+//        addEventListener("turbolinks:before-render", function() {
+//            out = document.getElementById("scroll");
+//            level = out.scrollTop;
+//            alert(level);
+//        });
+//
+//        addEventListener("turbolinks:render", function() {
+//            out.scrollTop = level;
+//        });
+//    });
 });
