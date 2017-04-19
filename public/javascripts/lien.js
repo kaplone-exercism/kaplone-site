95c281282ed6dcd5f3edf7d0c3882df6e0884edd
David <dvd.brtrnd@gmail.com>
2017-04-19 18:58:12 +0200

    cv par défaut

--- public/javascripts/lien.js
+++ public/javascripts/lien.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
 
     $('a.lien').click(function () {
-        alert($(this).attr("id"));
         $("#includedContent").load("/assets/articles/" + $(this).attr("id") + ".html");
         $('ul.tree').hide(600);
         $("a[id='" + $(this).attr("id") + "']").parent().parent().toggle(600);
