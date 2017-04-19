e4fdfaa81bdcc56c466a72c29bc1d38005a541a6
David <dvd.brtrnd@gmail.com>
2017-04-19 18:59:38 +0200

    cv par défaut

--- public/javascripts/leftColumn.js
+++ public/javascripts/leftColumn.js
@@ -6,7 +6,6 @@ $(document).ready(function () {
     });
 
     $('a.cv').click(function () {
-        alert($(this).attr("id"));
         $("#includedContent").load("/assets/articles/" + $(this).attr("id") + ".html");
         $('ul.tree').hide(600);
         $("a[id='" + $(this).attr("id") + "']").parent().parent().toggle(600);
