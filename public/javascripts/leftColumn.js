bc5b2110908868e30f437172e0a0903e28ef42ad
David <dvd.brtrnd@gmail.com>
2017-04-19 15:32:57 +0200

    cv par défaut

--- public/javascripts/leftColumn.js
+++ public/javascripts/leftColumn.js
@@ -3,20 +3,17 @@ $(document).ready(function () {
 
     $('a.menu').click(function () {
         $("#includedContent").load("/assets/articles/" + $(this).attr("id") + ".html");
-        alert("/assets/articles/" + $(this).attr("id") + ".html");
     });
 
     $('a.cv').click(function () {
         $("#includedContent").load("/assets/articles/" + $(this).attr("id") + ".html");
         $('ul.tree').hide(600);
-        $("a#" + $(this).attr("id")).parent().children('ul.tree').toggle(600);
-        alert("/assets/articles/" + $(this).attr("id") + ".html");
+        $("a[id='" + $(this).attr("id") + ']").parent().children('ul.tree').toggle(600);
     });
 
     $('label.tree-toggler').click(function () {
         $('ul.tree').hide(600);
         $(this).parent().children('ul.tree').toggle(600);
-        alert("toggle");
     });
 });
 
