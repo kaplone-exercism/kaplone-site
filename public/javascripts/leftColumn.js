9668ca733442905c732878e6e7eaa0094dfc86f8
David <dvd.brtrnd@gmail.com>
2017-04-19 15:52:44 +0200

    cv par défaut

--- public/javascripts/leftColumn.js
+++ public/javascripts/leftColumn.js
@@ -8,10 +8,7 @@ $(document).ready(function () {
     $('a.cv').click(function () {
         $("#includedContent").load("/assets/articles/" + $(this).attr("id") + ".html");
         $('ul.tree').hide(600);
-        $("a[id='" + $(this).attr("id") + "']").parent().toggle(600);
-        alert("a[id='" + $(this).attr("id") + "']");
-        alert($("a[id='" + $(this).attr("id") + "']").attr("class"));
-        alert($("a[id='" + $(this).attr("id") + "']").parent().parent().attr("class"));
+        $("a[id='" + $(this).attr("id") + "']").parent().parent().toggle(600);
     });
 
     $('label.tree-toggler').click(function () {
