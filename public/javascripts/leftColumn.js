e518d623286a8aa527fc6c86e75e6f321e498bf6
David <dvd.brtrnd@gmail.com>
2017-04-19 11:39:19 +0200

    test menu qui suit l'action

--- public/javascripts/leftColumn.js
+++ public/javascripts/leftColumn.js
@@ -1,14 +1,15 @@
 $(document).ready(function () {
     $('ul.tree').hide();
+
+    $('a.menu').click(function () {
+        //$('ul.tree').hide();
+        //$(this).parent().parent().children('label.tree-toggler').toggle(600);
+        $("#includedContent").load("/assets/images/articles/"+$(this).attr("id"));
+    });
+
+    $('label.tree-toggler').click(function () {
+        $('ul.tree').hide(600);
+        $(this).parent().children('ul.tree').toggle(600);
+    });
 });
 
-$('a.menu').click(function () {
-    //$('ul.tree').hide();
-    //$(this).parent().parent().children('label.tree-toggler').toggle(600);
-    $("#includedContent").load("/assets/images/articles/"+$(this).attr("id"));
-});
-
-$('label.tree-toggler').click(function () {
-    $('ul.tree').hide(600);
-    $(this).parent().children('ul.tree').toggle(600);
-});
