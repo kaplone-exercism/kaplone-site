5597780ecccbbe025981d4abe2ef38b5978a05ac
David <dvd.brtrnd@gmail.com>
2017-04-19 19:12:39 +0200

    cv par défaut

--- public/javascripts/leftColumn.js
+++ public/javascripts/leftColumn.js
@@ -9,6 +9,7 @@ $(document).ready(function () {
         $("#includedContent").load("/assets/articles/" + $(this).attr("id") + ".html");
         $('ul.tree').hide(600);
         $("a[id='" + $(this).attr("id") + "']").parent().parent().toggle(600);
+        window.scrollTo(0, 0);
     });
 
     $('label.tree-toggler').click(function () {
--- public/javascripts/lien.js
+++ public/javascripts/lien.js
@@ -4,5 +4,6 @@ $(document).ready(function () {
         $("#includedContent").load("/assets/articles/" + $(this).attr("id") + ".html");
         $('ul.tree').hide(600);
         $("a[id='" + $(this).attr("id") + "']").parent().parent().toggle(600);
+        window.scrollTo(0, 0);
     });
 });
