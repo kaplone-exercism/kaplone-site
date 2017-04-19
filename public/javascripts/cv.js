3a3fef360d5315355d12ce087006cd40b1cac778
David <dvd.brtrnd@gmail.com>
2017-04-19 15:15:05 +0200

    cv par défaut

--- public/javascripts/cv.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-
-    $('a').click(function () {
-        $("#includedContent").load("/assets/articles/" + $(this).attr("id") + ".html");
-        $('ul.tree').hide(600);
-        $("a#" + $(this).attr("id")).parent().children('ul.tree').toggle(600);
-    });
-});
-
