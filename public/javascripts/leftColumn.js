631da37bceee1e519f04db524222225c4835eeb4
David <dvd.brtrnd@gmail.com>
2017-04-20 20:43:09 +0200

    Ajout home et expand all

--- public/javascripts/leftColumn.js
+++ public/javascripts/leftColumn.js
@@ -21,12 +21,14 @@ $(document).ready(function () {
 
     $('a.expand').click(function() {
 
+        alert($(this.children("img")).attr("src"));
+
         if ($(this.children("img")).attr("src") == "/assets/images/TreeView.gif"){
             $('ul.tree').hide(60);
             $(this).html($.parseHTML('<img src="/assets/images/icon-65-128.png" height="42" width="42"><label class="tree-toggler">Replier tout</label>'));
         }
         else {
-            $(this).parent().html($.parseHTML('<img src="/assets/images/TreeView.gif" height="42" width="42"><label class="tree-toggler">Déplier tout</label>'));
+            $(this).html($.parseHTML('<img src="/assets/images/TreeView.gif" height="42" width="42"><label class="tree-toggler">Déplier tout</label>'));
         }
 
         $('ul.tree').toggle(600);
