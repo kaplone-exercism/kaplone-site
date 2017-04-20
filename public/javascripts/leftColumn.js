86a96656751caa56d053e512a6a36ace118a47ae
David <dvd.brtrnd@gmail.com>
2017-04-20 19:38:45 +0200

    Ajout home et expand all

--- public/javascripts/leftColumn.js
+++ public/javascripts/leftColumn.js
@@ -12,6 +12,18 @@ $(document).ready(function () {
         window.scrollTo(0, 0);
     });
 
+    $('a.home').click(function() {
+        $('ul.tree').hide(600);
+        $("#includedContent").load("/assets/articles/cv.html");
+        window.scrollTo(0, 0);
+    });
+
+    $('a.expand').click(function() {
+        $('ul.tree').hide(60);
+        $('ul.tree').toggle(600);
+        window.scrollTo(0, 0);
+    });
+
     $('label.tree-toggler').click(function () {
         $('ul.tree').hide(600);
         $(this).parent().children('ul.tree').toggle(600);
