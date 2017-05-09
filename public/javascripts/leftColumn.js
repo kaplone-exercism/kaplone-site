eb5a4d79ca1256892019ecda136d1efb7a1c2f20
David <dvd.brtrnd@gmail.com>
2017-05-09 23:57:24 +0200

    Ajout d'articles

--- public/javascripts/leftColumn.js
+++ public/javascripts/leftColumn.js
@@ -6,12 +6,12 @@ $(document).ready(function () {
         window.scrollTo(0, 0);
     });
 
-    $('a.cv').click(function () {
+    /*$('a.cv').click(function () {
         $("#includedContent").load("/assets/articles/" + $(this).attr("id") + ".html");
         $('ul.tree').hide(600);
         $("a[id='" + $(this).attr("id") + "']").parent().parent().toggle(600);
         window.scrollTo(0, 0);
-    });
+    });*/
 
     $('a.home').click(function() {
         $('ul.tree').hide(600);
