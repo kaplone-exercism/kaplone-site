307c13e3b9a8edeecfbcfee4e1cf1d65764d6c71
David <dvd.brtrnd@gmail.com>
2017-05-09 23:53:19 +0200

    Ajout d'articles

--- public/javascripts/lien.js
+++ public/javascripts/lien.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    $('a.lien').click(function () {
+    $('a.cv').click(function () {
         $("#includedContent").load("/assets/articles/" + $(this).attr("id") + ".html");
         $('ul.tree').hide(600);
         $("a[id='" + $(this).attr("id") + "']").parent().parent().toggle(600);
