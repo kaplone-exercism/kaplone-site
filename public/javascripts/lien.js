51b86d8f4ef412beccc36d4a8905386bf26735fe
David <dvd.brtrnd@gmail.com>
2017-05-10 00:03:46 +0200

    Ajout d'articles

--- public/javascripts/lien.js
+++ public/javascripts/lien.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
     $('a.lien').click(function () {
         $("#includedContent").load("/assets/articles/" + $(this).attr("id") + ".html");
-        $('ul.tree').hide();
+        $('ul.tree').hide(200);
         $("a[id='" + $(this).attr("id") + "']").parent().parent().toggle(600);
         window.scrollTo(0, 0);
     });
