3d978ae3a3d1120f2a96d171d2c4cbdb6658dbaa
David <dvd.brtrnd@gmail.com>
2017-05-11 15:33:29 +0200

    Ajout d'articles

--- public/javascripts/cv.js
+++ public/javascripts/cv.js
@@ -5,5 +5,25 @@ $(document).ready(function () {
         $('ul.tree').hide(600);
         $("a[id='" + $(this).attr("id") + "']").parent().parent().toggle(600);
         window.scrollTo(0, 0);
+        $('#updated').text(fetchHeader("/assets/articles/" + $(this).attr("id") + ".html",'Last-Modified'));
     });
+
+    function fetchHeader(url, wch) {
+            try {
+                var req=new XMLHttpRequest();
+                req.open("HEAD", url, false);
+                req.send(null);
+                if(req.status== 200){
+                    var derniereModif= req.getResponseHeader(wch);
+                    var dateModif = new Date(derniereModif);
+                    var jour = dateModif.getDate();
+                    var mois = dateModif.getMonth() + 1;
+                    var annee = dateModif.getYear() + 1900;
+                    return "Dernière modification de la page le " + jour + "/" + mois + "/" + annee;
+                }
+                else return false;
+            } catch(er) {
+                return er.message;
+            }
+        }
 });
