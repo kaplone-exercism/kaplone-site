ddd80026f91b0aeaacf4d2424cc578f4ba2c9d35
David <dvd.brtrnd@gmail.com>
2017-05-11 15:33:47 +0200

    Ajout d'articles

--- public/javascripts/leftColumn.js
+++ public/javascripts/leftColumn.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
     $('a.menu').click(function () {
         $("#includedContent").load("/assets/articles/" + $(this).attr("id") + ".html");
         window.scrollTo(0, 0);
+        $('#updated').text(fetchHeader("/assets/articles/" + $(this).attr("id") + ".html",'Last-Modified'));
     });
 
     $('a.home').click(function() {
@@ -11,6 +12,7 @@ $(document).ready(function () {
         $("#includedContent").load("/assets/articles/cv.html");
         $('a.expand').html($.parseHTML('<img src="/assets/images/TreeView.gif" height="42" width="42"><label class="tree-toggler">Déplier tout</label>'));
         window.scrollTo(0, 0);
+        $('#updated').text(fetchHeader("/assets/articles/cv.html",'Last-Modified'));
     });
 
     $('a.expand').click(function() {
@@ -34,5 +36,24 @@ $(document).ready(function () {
             $('ul.tree').hide(600);
         }
     });
+
+    function fetchHeader(url, wch) {
+            try {
+                var req=new XMLHttpRequest();
+                req.open("HEAD", url, false);
+                req.send(null);
+                if(req.status== 200){
+                    var derniereModif= req.getResponseHeader(wch);
+                    var dateModif = new Date(derniereModif);
+                    var jour = dateModif.getDate();
+                    var mois = dateModif.getMonth() + 1;
+                    var annee = dateModif.getYear() + 1900;
+                    return "Dernière modification de la page le " + jour + "/" + mois + "/" + annee;
+                }
+                else return false;
+            } catch(er) {
+                return er.message;
+            }
+        }
 });
 
