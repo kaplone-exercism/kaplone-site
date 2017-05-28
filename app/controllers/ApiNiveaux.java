75f55453d8c972a96a946cd316d53fc340a48ad5
David <dvd.brtrnd@gmail.com>
2017-05-28 11:02:20 +0200

    Ajout d'articles

--- app/controllers/ApiNiveaux.java
+++ app/controllers/ApiNiveaux.java
@@ -55,7 +55,7 @@ public class ApiNiveaux extends Controller {
             e.printStackTrace();
         }
 
-        return "problème avec l'utilisateur ..."
+        return "problème avec l'utilisateur ...";
     }
 
 }
