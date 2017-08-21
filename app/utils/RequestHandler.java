d1cb0aab79ea4357791ae2a0bb4a7d873f589284
David <dvd.brtrnd@gmail.com>
2017-08-21 09:52:28 +0200

    lancement laserStrikes

--- app/utils/RequestHandler.java
+++ app/utils/RequestHandler.java
@@ -12,19 +12,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.jongo.MongoCursor;
-
-import com.mongodb.MongoTimeoutException;
-
 /**
  * Ecouteur du socket coté serveur.
  */
 class RequestHandler extends Thread{
 
 	private Socket socket;
-	
-	private MongoCursor<Enregistrable> cursor_e;
-	private MongoCursor<Destinataire> cursor_d;
 
 	/**
 	 * Constructeur canonique
@@ -68,83 +61,7 @@ class RequestHandler extends Thread{
 					out.flush();
 
 				    break;
-
-				case "0" : if (line.split("&").length == 3 && !line.split("&")[2].equals("")){
-
-					ArrayList<String> retour_array = new ArrayList<>();
-					String retour;
-
-					cursor_e = MongoAccess.requestAnyMatch(line.split("&")[0], line.split("&")[1], line.split("&")[2], true).as(Enregistrable.class);
-                    
-					
-					if (cursor_e != null){
-						while (cursor_e.hasNext()){
-							retour_array.add(cursor_e.next().getNom());
-						}
-					}
-					retour = retour_array.stream().sorted().collect(Collectors.joining("&"));
-
-					System.out.println("retour : " + retour);
-
-					out.write(retour);
-					out.newLine();
-					out.flush();
-				}   
-
-				else{
-
-					ArrayList<String> retour_array = new ArrayList<>();
-					String retour;
-
-					cursor_e = MongoAccess.request(line.split("&")[0]).as(Enregistrable.class);
-
-
-					if (cursor_e != null){
-						while (cursor_e.hasNext()){
-							retour_array.add(cursor_e.next().getNom());
-						}
-					}
-
-					retour = retour_array.stream().sorted().collect(Collectors.joining("&"));
-
-					System.out.println("retour : " + retour);
-
-					out.write(retour);
-					out.newLine();
-					out.flush();
-				} 
-				break;
-
-
-				case "1" :
-					ArrayList<String> retour_array = new ArrayList<>();
-					String retour;
-
-					Materiel materiel = MongoAccess.request("materiel", "nom", line).as(Materiel.class);
-
-					cursor_d = MongoAccess.requestIn("destinataire", materiel.getTags()).as(Destinataire.class);
-
-                    if (cursor_d != null){
-                    	while (cursor_d.hasNext()){
-    						Destinataire d = cursor_d.next();
-    						System.out.println("destinataire.getNom() : " + d.getNom());
-    						retour_array.add(d.getNom());
-    					}
-                    }
-				
-					retour = retour_array.stream().collect(Collectors.joining("&"));
-
-					System.out.println("retour : " + retour);
-
-					out.write(retour);
-					out.newLine();
-					out.flush();
-
-					break;
 				}
-
-
-
 				line = in.readLine();
 			}
 
@@ -160,8 +77,6 @@ class RequestHandler extends Thread{
         	System.out.println("exception dans requestHandler : EOFException");
         	try {
 				this.socket.close();
-				this.cursor_e.close();
-				this.cursor_d.close();
 			} catch (IOException | NullPointerException e) {
 				e.printStackTrace();
 			}
@@ -172,26 +87,12 @@ class RequestHandler extends Thread{
 			System.out.println("exception dans requestHandler : NullPointerException");
 			try {
 				this.socket.close();
-				this.cursor_e.close();
-				this.cursor_d.close();
 			} catch (IOException | NullPointerException e) {
 				e.printStackTrace();
 			}
 			npe.printStackTrace();
 		}
-		
-		catch (MongoTimeoutException mte){
-			System.out.println("exception dans requestHandler : MongoTimeoutException");
-			try {
-				this.socket.close();
-				this.cursor_e.close();
-				this.cursor_d.close();
-			} catch (IOException | NullPointerException e) {
-				e.printStackTrace();
-			}
-			mte.printStackTrace();
-		}
-		
+
 		catch( Exception e )
 		{
 			e.printStackTrace();
@@ -203,22 +104,6 @@ class RequestHandler extends Thread{
 				try {
 					socket.close();
 				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			if (cursor_e != null){
-				try {
-					cursor_e.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-			if (cursor_d != null){
-				try {
-					cursor_d.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
